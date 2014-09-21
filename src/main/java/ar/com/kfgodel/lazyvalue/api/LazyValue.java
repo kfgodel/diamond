ecedcd8ff632c6d40d2ba8082759243cd6ec49af
Dario L Garcia <dario.garcia@kfgodel_dev>
2014-09-21 16:32:48 -0300

    Added lazyValues for class parts

---
+++ src/main/java/ar/com/kfgodel/lazyvalue/api/LazyValue.java
@@ -0,0 +1,27 @@
+package ar.com.kfgodel.lazyvalue.api;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+/**
+ * This type represents a value that is defined the first time is needed
+ * Created by kfgodel on 21/09/14.
+ */
+public interface LazyValue<T> extends Supplier<T> {
+    /**
+     * Indicates if this value has already been defined
+     * @return True if the generator expression was evaluated and the value defined
+     */
+    boolean isAlreadyDefined();
+
+    /**
+     * @return The generator lambda that is going to be used to define the value of this instance.<br>
+     *     En empty optional is returned if the value is already defined and the lambda discarded
+     */
+    Optional<Supplier<T>> generator();
+
+    /**
+     * @return THe optional value if defined, an empty optional if not
+     */
+    Optional<T> getIfDefined();
+}
---
+++ src/test/java/ar/com/kfgodel/diamond/value/LazyValueTest.java
@@ -0,0 +1,57 @@
+package ar.com.kfgodel.diamond.value;
+
+import ar.com.dgarcia.javaspec.api.JavaSpec;
+import ar.com.dgarcia.javaspec.api.JavaSpecRunner;
+import ar.com.kfgodel.diamond.DiamondTestContext;
+import ar.com.kfgodel.lazyvalue.impl.SuppliedValue;
+import org.junit.runner.RunWith;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * This type test the behavior of a lazily defined value
+ * Created by kfgodel on 21/09/14.
+ */
+@RunWith(JavaSpecRunner.class)
+public class LazyValueTest extends JavaSpec<DiamondTestContext> {
+    @Override
+    public void define() {
+        describe("a lazy value", ()->{
+            Supplier<Integer> generator = () -> 42;
+            beforeEach(()->{
+                context().value(() -> SuppliedValue.create(generator));
+            });
+
+            it("has an undefined value", ()->{
+                assertThat(context().value().isAlreadyDefined())
+                        .isFalse();
+            });
+            it("has a supplier lambda to define its value", ()->{
+                assertThat(context().value().generator().get()).
+                        isEqualTo(generator);
+            });
+            it("value is defined the first time it's accessed", ()->{
+                Integer generated = context().value().get();
+                assertThat(generated).isEqualTo(42);
+            });
+            it("value it's retained once defined", ()->{
+                Integer firstTime = context().value().get();
+                Integer secondTime = context().value().get();
+                assertThat(firstTime).isSameAs(secondTime);
+            });
+            it("supplier lambda is discarded once defined, to prevent unintentional memory leaks", ()->{
+                context().value().get();
+                Optional<Supplier<Integer>> discardedLambda = context().value().generator();
+                assertThat(discardedLambda.isPresent()).isFalse();
+            });
+
+            it("value can be optionally accessed if not yet defined", ()->{
+                Optional<Integer> optionalValue = context().value().getIfDefined();
+                assertThat(optionalValue.isPresent()).isFalse();
+            });
+        });
+    }
+}
