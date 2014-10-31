27c2d9e65294855f70cff0ddfb16774a5dc511fa
Dario L Garcia <dario.garcia@kfgodeldev.com>
2014-10-30 23:49:22 -0300

    AsFunction added to simplify functional conversions

---
+++ src/main/java/ar/com/kfgodel/diamond/impl/invokables/UndefinedInvoker.java
@@ -0,0 +1,28 @@
+package ar.com.kfgodel.diamond.impl.invokables;
+
+import ar.com.kfgodel.diamond.api.exceptions.DiamondException;
+import ar.com.kfgodel.diamond.api.invokable.PolymorphicInvokable;
+import ar.com.kfgodel.diamond.api.members.TypeMember;
+
+import java.util.function.Supplier;
+
+/**
+ * This type represents an undefined value for a type member
+ * Created by kfgodel on 30/10/14.
+ */
+public class UndefinedInvoker implements Supplier<PolymorphicInvokable> {
+
+    private TypeMember member;
+
+    @Override
+    public PolymorphicInvokable get() {
+        throw new DiamondException("This member["+member+"] doesn't define an invoker");
+    }
+
+    public static UndefinedInvoker create(TypeMember member) {
+        UndefinedInvoker invoker = new UndefinedInvoker();
+        invoker.member = member;
+        return invoker;
+    }
+
+}
