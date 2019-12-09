583da5271293407248daf2757e9dcf48a488c83f
Dario L Garcia <dario.garcia@10pines.com>
2019-12-09 12:12:40 -0300

    Adjust line lengths

--- src/test/java/ar/com/kfgodel/diamond/unit/types/ParameterizedTypeTest.java
+++ src/test/java/ar/com/kfgodel/diamond/unit/types/ParameterizedTypeTest.java
@@ -1,6 +1,8 @@
 package ar.com.kfgodel.diamond.unit.types;
 
 import ar.com.kfgodel.diamond.api.Diamond;
+import ar.com.kfgodel.diamond.api.annotations.Annotated;
+import ar.com.kfgodel.diamond.api.naming.Named;
 import ar.com.kfgodel.diamond.api.types.TypeInstance;
 import ar.com.kfgodel.diamond.api.types.reference.ReferenceOf;
 import ar.com.kfgodel.diamond.unit.DiamondTestContext;
@@ -16,6 +18,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,7 +36,7 @@ public class ParameterizedTypeTest extends JavaSpec<DiamondTestContext> {
     describe("a parameterized type representation", () -> {
 
       beforeEach(() -> {
-        context().typeInstance(() -> createParameterizedType());
+        context().typeInstance(ParameterizedTypeTest::createParameterizedType);
       });
 
       it("has a name", () -> {
@@ -43,53 +46,71 @@ public class ParameterizedTypeTest extends JavaSpec<DiamondTestContext> {
 
       it("has a declaration", () -> {
         assertThat(context().typeInstance().declaration())
-          .isEqualTo("@ar.com.kfgodel.diamond.unit.testobjects.annotations.TestAnnotation1() ar.com.kfgodel.diamond.unit.testobjects.lineage.ParentClass<@ar.com.kfgodel.diamond.unit.testobjects.annotations.TestAnnotation2() java.lang.String, @ar.com.kfgodel.diamond.unit.testobjects.annotations.TestAnnotation3() java.lang.Integer>");
+          .isEqualTo("@ar.com.kfgodel.diamond.unit.testobjects.annotations.TestAnnotation1() " +
+            "ar.com.kfgodel.diamond.unit.testobjects.lineage.ParentClass<" +
+            "@ar.com.kfgodel.diamond.unit.testobjects.annotations.TestAnnotation2() " +
+            "java.lang.String, @ar.com.kfgodel.diamond.unit.testobjects.annotations.TestAnnotation3() " +
+            "java.lang.Integer>");
       });
 
       it("has type parameters", () -> {
-        List<String> typeParameterNames = context().typeInstance().generics().parameters().map((typeParameter) -> typeParameter.name()).collect(Collectors.toList());
+        List<String> typeParameterNames = context().typeInstance()
+          .generics().parameters()
+          .map(Named::name)
+          .collect(Collectors.toList());
         assertThat(typeParameterNames)
           .isEqualTo(Arrays.asList("P1", "P2"));
       });
 
 
       it("has type arguments", () -> {
-        List<String> argumentNames = context().typeInstance().generics().arguments().map((typeArgument) -> typeArgument.name()).collect(Collectors.toList());
+        List<String> argumentNames = context().typeInstance()
+          .generics().arguments()
+          .map(Named::name)
+          .collect(Collectors.toList());
         assertThat(argumentNames).isEqualTo(Arrays.asList("String", "Integer"));
       });
 
       it("type argument declaration names are preserved", () -> {
-        List<String> argumentDeclarations = context().typeInstance().generics().arguments()
-          .map((typeArgument) -> typeArgument.declaration())
+        List<String> argumentDeclarations = context().typeInstance()
+          .generics().arguments()
+          .map(TypeInstance::declaration)
           .collect(Collectors.toList());
         assertThat(argumentDeclarations)
-          .isEqualTo(Arrays.asList("@ar.com.kfgodel.diamond.unit.testobjects.annotations.TestAnnotation2() java.lang.String", "@ar.com.kfgodel.diamond.unit.testobjects.annotations.TestAnnotation3() java.lang.Integer"));
+          .isEqualTo(Arrays.asList("@ar.com.kfgodel.diamond.unit.testobjects.annotations.TestAnnotation2() " +
+            "java.lang.String",
+            "@ar.com.kfgodel.diamond.unit.testobjects.annotations.TestAnnotation3() java.lang.Integer"));
 
       });
 
       it("can have attached annotations", () -> {
         List<Class<? extends Annotation>> annotationTypes = context().typeInstance().annotations()
-          .map((annotation) -> annotation.annotationType())
+          .map(Annotation::annotationType)
           .collect(Collectors.toList());
         assertThat(annotationTypes)
-          .isEqualTo(Arrays.asList(TestAnnotation1.class));
+          .isEqualTo(Collections.singletonList(TestAnnotation1.class));
       });
 
       it("type arguments can have attached annotations too", () -> {
-        List<Class<? extends Annotation>> annotationTypes = context().typeInstance().generics().arguments()
-          .flatMap((typeArgument) -> typeArgument.annotations())
-          .map((annotation) -> annotation.annotationType())
+        List<Class<? extends Annotation>> annotationTypes = context().typeInstance()
+          .generics().arguments()
+          .flatMap(Annotated::annotations)
+          .map(Annotation::annotationType)
           .collect(Collectors.toList());
         assertThat(annotationTypes)
-          .isEqualTo(Arrays.asList(TestAnnotation2.class, TestAnnotation3.class));
+          .isEqualTo(Arrays.asList(
+            TestAnnotation2.class,
+            TestAnnotation3.class
+          ));
       });
 
       /**
        * This is due to the lack of an annotated type parameters method on the Class instance
        */
       it("type parameters can't have attached annotations", () -> {
-        long parameterAnnotationsCount = context().typeInstance().generics().parameters()
-          .flatMap((typeArgument) -> typeArgument.annotations())
+        long parameterAnnotationsCount = context().typeInstance()
+          .generics().parameters()
+          .flatMap(Annotated::annotations)
           .count();
         // Even though the 1st type parameter has annotations we cannot access it on runtime
         // with reflection. Limitation from current reflection API
